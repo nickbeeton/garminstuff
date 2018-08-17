fe7a8c94cba1e6775c8e095a5f5e8eaca4f9dcad
nickbeeton <nick.beeton@utas.edu.au>
2018-08-17 15:25:17 +1000

    Update bulk summaries.js

--- bulk summaries.js
+++ bulk summaries.js
@@ -1,3 +1,10 @@
+/*
+Use the same download location in your browser settings as for the other Javascript.
+
+First Navigate to the last (most recent) daily summary you have in Garmin Connect (as in https://connect.garmin.com/modern/daily-summary/Bob/2018-08-17/timeline ), then hit F12 (should work in chrome/IE) to open dev tools to get to the Javascript Console. Then paste the below code and hit enter to run it. Can change ttl from 300 to whatever # of days you want to download.
+If your connection is too slow to do a full download in less than 3 seconds every time, change the downloadTimeoutLength from 3 * 1000 to whatever number you want (it's 3*1000 because that's 3000 milliseconds = 3 seconds).
+
+[CODE]*/
 var a = window.location.pathname.split("/");
 var id = a[a.length-2];
 var da = new Date(id);
