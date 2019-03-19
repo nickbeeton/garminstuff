2ee128101c53a723fc710045254ebdccc8c70df6
nickbeeton <nick.beeton@data61.csiro.au>
2019-03-19 12:25:47 +1100

    Restrict to only logged-in athlete
    Fix script attempting to change bike rides for other athletes! (oops)

--- Convert bike rides to ebike rides on Strava.user.js
+++ Convert bike rides to ebike rides on Strava.user.js
@@ -9,20 +9,22 @@
 // ==/UserScript==
 
 window.f = function(i){
-    if (document.getElementsByClassName('icon-ride icon-dark').length > 1){ // if rides detected
-        if (document.getElementsByClassName('icon-ride icon-dark')[i].title === "")	{ // if it's not just the ride goal logo
-            var window2 = open(window.document.getElementsByClassName('icon-ride icon-dark')[i].parentElement.parentElement.nextElementSibling.firstElementChild.href+"/edit"); // open a new edit window for the ride
-            window2.focus();
-            window2.onload = function() { // once it's loaded, change it to e-bike and save
-                var drop = window2.document.getElementById('activity_type');
-                drop.value = "EBikeRide";
-                window2.document.getElementsByClassName('btn-save-activity')[0].click();
-                setTimeout((window.g = function(){ // then after a delay, close the window and start the proces again for the next ride
-                    window2.close();
-                    window.f(i+1);
-                }), 10000); // amount of time (in ms) to wait till starting on the next file
-            };
-        };
-    };
+    if (document.getElementsByClassName("selection")[3].href === window.location.href){ // if this is YOUR page
+		if (document.getElementsByClassName('icon-ride icon-dark').length > 1){ // if rides detected
+			if (document.getElementsByClassName('icon-ride icon-dark')[i].title === "")	{ // if it's not just the ride goal logo
+				var window2 = open(window.document.getElementsByClassName('icon-ride icon-dark')[i].parentElement.parentElement.nextElementSibling.firstElementChild.href+"/edit"); // open a new edit window for the ride
+				window2.focus();
+				window2.onload = function() { // once it's loaded, change it to e-bike and save
+					var drop = window2.document.getElementById('activity_type');
+					drop.value = "EBikeRide";
+					window2.document.getElementsByClassName('btn-save-activity')[0].click();
+					setTimeout((window.g = function(){ // then after a delay, close the window and start the proces again for the next ride
+						window2.close();
+						window.f(i+1);
+					}), 10000); // amount of time (in ms) to wait till starting on the next file
+				};
+			};
+		};
+	};
 };
 window.f(0);
